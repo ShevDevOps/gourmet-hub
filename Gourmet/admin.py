f4fe04664b9663274ef677b7f49f33717f07b40d
Matyuk <Stivano@ukr.net>
2025-04-25 12:34:17 +0300

    Створив таблицю адмін + почав сторінку orders

---
+++ Gourmet/admin.py
@@ -0,0 +1,2 @@
+from django.contrib import admin
+from order_table.models import *
--- order_table/views.py
+++ order_table/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+from order_table.models import *
 
 # Create your views here.
+def order_table(request):
+    tables = Table.objects.all()
+    return render(request, 'order_table.html', {'tables': tables})
