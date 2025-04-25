e9ebf627cacdd1a9bba5071cfd764f568f31b8e4
Matyuk <Stivano@ukr.net>
2025-04-25 09:05:11 +0300

    Додав Замовлені столики та замовленні блюда, додав заглушку для блюд

--- dishes/models.py
+++ dishes/models.py
@@ -1,3 +1,5 @@
 from django.db import models
 
 # Create your models here.
+
+class Dish(models.Model): pass # Placeholder for the Dish model for order_table/models.py -- Tryhub
--- order_table/models.py
+++ order_table/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.core.exceptions import ValidationError
+from dishes.models import Dish
 
 class Table(models.Model):
     table_number = models.IntegerField(unique=True)
@@ -16,3 +18,23 @@ class Client(models.Model):
     
     def __str__(self):
         return self.name
+
+class Ordered_Table(Table):
+    Client_Id = models.ForeignKey(Client, on_delete=models.CASCADE)
+    Client_num = models.IntegerField()
+    Order_time = models.DateTimeField()
+
+    def clean(self):
+        if self.capacity < self.Client_num:
+            raise ValidationError("Client number exceeds table capacity.")
+        super().clean()
+
+    def __str__(self):
+        return self.name
+    
+class DishToTable(models.Model):
+    table = models.ForeignKey(Ordered_Table, on_delete=models.CASCADE)
+    Dish = models.ForeignKey(Dish, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"Order for {self.table} at {self.order_time}"
