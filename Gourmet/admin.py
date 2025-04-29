07a44c18498fe48340e26e0822e15f06843b9d52
Matyuk <Stivano@ukr.net>
2025-04-29 11:14:25 +0300

    Додав моделі в dishes i shefs і зробив міграції

--- Gourmet/admin.py
+++ Gourmet/admin.py
@@ -1,7 +1,12 @@
 from django.contrib import admin
 from order_table.models import *
+from dishes.models import *
+from shefs.models import *
 
 admin.site.register(Table)
 admin.site.register(Ordered_Table)
 admin.site.register(Client)
-admin.site.register(DishToTable)
+admin.site.register(Dish)
+admin.site.register(Ingridient)
+admin.site.register(Chef)
+admin.site.register(ChefToDish)
--- shefs/models.py
+++ shefs/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from dishes.models import Dish
 
 # Create your models here.
+class Chef(models.Model): 
+    name = models.CharField(max_length=100)
+    description = models.TextField(null=True, blank=True)
+    image = models.ImageField(upload_to='chef_images/', null=True, blank=True)
+
+class ChefToDish(models.Model):
+    chef = models.ForeignKey(Chef, on_delete=models.CASCADE)
+    dish = models.ForeignKey(Dish, on_delete=models.CASCADE)
