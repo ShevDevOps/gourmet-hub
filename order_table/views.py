6c5cd25c4c2ac5e14b82402d2d981ca6e2c273fb
Matyuk <Stivano@ukr.net>
2025-05-02 08:56:05 +0300

    Підтянув css до order

--- order_table/views.py
+++ order_table/views.py
@@ -6,5 +6,10 @@ from django.contrib.auth.decorators import login_required
 # # Create your views here.
 # @login_required(login_url='login')  # ЗРОБИТИ РЕГІСТРАЦІЮ І АВТОРИЗАЦІЮ
 def order_table(request):
+    if request.method == 'POST':
+        table_id = request.POST.get('table_id')
+        table = Table.objects.get(id=table_id)
+        order = Ordered_Table.objects.create(table=table)
+        return JsonResponse({'order_id': order.id})
     tables = Table.objects.all()
     return render(request, 'order.html', {'tables': tables})
